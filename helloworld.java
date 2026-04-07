7bb6452d6582cf773b8db5f94c89d8142c4754ba
jongar <jongarcicuevas@gmail.com>
2026-04-07 12:57:06 +0200

    code refactoring: unnecessarily complicated!

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,25 @@
 public class HelloWorld {
+    
+    private static final String DEFAULT_GREETING = "Hello World!!!!";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        Greeter greeter = new Greeter(DEFAULT_GREETING);
+        greeter.sayHello();
+    }
+}
+
+class Greeter {
+    private final String message;
+
+    public Greeter(String message) {
+        this.message = message;
+    }
+
+    public void sayHello() {
+        if (message == null || message.isEmpty()) {
+            System.out.println("... (Silence) ...");
+        } else {
+            System.out.println(message);
+        }
     }
 }
